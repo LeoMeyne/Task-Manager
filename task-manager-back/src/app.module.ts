7da2e12cd66a57cd7f0e2a3935244d366a1d9c34
LeoMeyne <leo.meyne@gmail.com>
2025-01-14 14:36:24 +0100

    Add modules

--- task-manager-back/src/app.module.ts
+++ task-manager-back/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { MessagingModule } from './messaging/messaging.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, TasksModule, NotificationsModule, MessagingModule],
   controllers: [AppController],
   providers: [AppService],
 })
