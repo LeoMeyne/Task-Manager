96820a67e5aeedf34506700b0d57572fb7ff2efc
LeoMeyne <leo.meyne@gmail.com>
2025-01-14 15:53:15 +0100

    Back and BD docker

--- task-manager-back/src/app.module.ts
+++ task-manager-back/src/app.module.ts
@@ -6,9 +6,26 @@ import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { MessagingModule } from './messaging/messaging.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UsersModule, TasksModule, NotificationsModule, MessagingModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    TasksModule,
+    NotificationsModule,
+    MessagingModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'taskapp',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
